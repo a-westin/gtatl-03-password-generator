0a48e089f303ea735e5121592d5648c22a0bed4d
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-09-14 00:14:30 -0400

    Declared variables, wrote functions, modified HTML. Not working yet.

--- script.js
+++ script.js
@@ -11,3 +11,85 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function getRandomUpperCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
+  return symbol[Math.floor(Math.random() * symbol.length)];
+}
+
+//Communicating with the DOM
+
+var passwordEl = document.getElementById("password");
+var lengthEl = document.getElementById("length");
+var numberEl = document.getElementById("number");
+var lowerEl = document.getElementById("lower");
+var upperEl = document.getElementById("upper");
+var symbolEl = document.getElementById("symbol");
+var generateEl = document.getElementById("generate");
+
+const randomFunc = {
+  upper: getRandomUpperCase,
+  lower: getRandomLowerCase,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+//generate event
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasUpper = upperEl.checked;
+  const hasLower = lowerEl.checked;
+  const hasNumber = numberEl.checked;
+  const hasSymbol = symbolEl.checked;
+
+  passwordEl.innerText = generatePassword(
+    hasUpper,
+    hasLower,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+//Generate Password Function
+function generatePassword(upper, lower, number, symbol, length) {
+  let generatedPassword = "";
+
+  const typesCount = upper + lower + number + symbol;
+  // if typesCount == 0 {
+  //   alert("You must check at least one box!")
+  // };
+
+  // console.log(typesCount);
+
+  const typesArr = [{ upper }, { lower }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
