42d75d402628ce54b938f4aa1bec189f1fb63046
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-09-15 20:46:43 -0400

    Tried a different function, returning undefined. Switched to a password length slider.

--- script.js
+++ script.js
@@ -12,84 +12,108 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function getRandomUpperCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+// //generate event
 
-function getRandomLowerCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+function generatePassword() {
+  let complexity = document.getElementById("slider").values;
 
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+  let values =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&*()-_=+[{]}|;:',<.>/?";
 
-function getRandomSymbol() {
-  var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
-  return symbol[Math.floor(Math.random() * symbol.length)];
-}
+  let passwordEL = "";
 
-//Communicating with the DOM
-
-var passwordEl = document.getElementById("password");
-var lengthEl = document.getElementById("length");
-var numberEl = document.getElementById("number");
-var lowerEl = document.getElementById("lower");
-var upperEl = document.getElementById("upper");
-var symbolEl = document.getElementById("symbol");
-var generateEl = document.getElementById("generate");
-
-const randomFunc = {
-  upper: getRandomUpperCase,
-  lower: getRandomLowerCase,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
-};
-
-//generate event
-generateEl.addEventListener("click", () => {
-  const length = +lengthEl.value;
-  const hasUpper = upperEl.checked;
-  const hasLower = lowerEl.checked;
-  const hasNumber = numberEl.checked;
-  const hasSymbol = symbolEl.checked;
-
-  passwordEl.innerText = generatePassword(
-    hasUpper,
-    hasLower,
-    hasNumber,
-    hasSymbol,
-    length
-  );
-});
-
-//Generate Password Function
-function generatePassword(upper, lower, number, symbol, length) {
-  let generatedPassword = "";
-
-  const typesCount = upper + lower + number + symbol;
-  // if typesCount == 0 {
-  //   alert("You must check at least one box!")
-  // };
-
-  // console.log(typesCount);
-
-  const typesArr = [{ upper }, { lower }, { number }, { symbol }].filter(
-    (item) => Object.values(item)[0]
-  );
-
-  if (typesCount === 0) {
-    return "";
+  for (var i = 0; i <= complexity; i++) {
+    passwordEL =
+      passwordEL +
+      values.charAt(Math.floor(Math.random() + Math.floor(value.length - 1)));
   }
 
-  for (let i = 0; i < length; i += typesCount) {
-    typesArr.forEach((type) => {
-      const funcName = Object.keys(type)[0];
-      generatedPassword += randomFunc[funcName]();
-    });
-  }
+  document.getElementById("length").innerHTML = "Length: 8";
 
-  const finalPassword = generatedPassword.slice(0, length);
-
-  return finalPassword;
+  document.getElementById("slider").oninput = function () {
+    document.getElementById("length").innerHTML =
+      "Length: " + document.getElementById("slider").value;
+  };
 }
+// generateEl.addEventListener("click", () => {
+//   const length = +lengthEl.value;
+//   const hasUpper = upperEl.checked;
+//   const hasLower = lowerEl.checked;
+//   const hasNumber = numberEl.checked;
+//   const hasSymbol = symbolEl.checked;
+
+//   passwordEl.innerText = generatePassword(
+//     hasUpper,
+//     hasLower,
+//     hasNumber,
+//     hasSymbol,
+//     length
+//   );
+// });
+
+// function getRandomUpperCase() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
+
+// function getRandomLowerCase() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
+
+// function getRandomNumber() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// }
+
+// function getRandomSymbol() {
+//   var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
+//   return symbol[Math.floor(Math.random() * symbol.length)];
+// }
+
+// //Communicating with the DOM
+
+// var passwordEl = document.getElementById("password");
+// var lengthEl = document.getElementById("length");
+// var numberEl = document.getElementById("number");
+// var lowerEl = document.getElementById("lower");
+// var upperEl = document.getElementById("upper");
+// var symbolEl = document.getElementById("symbol");
+// var generateEl = document.getElementById("generate");
+
+// const randomFunc = {
+//   upper: getRandomUpperCase,
+//   lower: getRandomLowerCase,
+//   number: getRandomNumber,
+//   symbol: getRandomSymbol,
+// };
+
+// for(var i = 0; i <=)
+
+// //Generate Password Function
+// // function generatePassword(upper, lower, number, symbol, length) {
+// //   let generatedPassword = "";
+
+// //   const typesCount = upper + lower + number + symbol;
+// //   // if typesCount == 0 {
+// //   //   alert("You must check at least one box!")
+// //   // };
+
+// //   // console.log(typesCount);
+
+// //   const typesArr = [{ upper }, { lower }, { number }, { symbol }].filter(
+// //     (item) => Object.values(item)[0]
+// //   );
+
+// //   if (typesCount === 0) {
+// //     return "";
+// //   }
+
+// //   for (let i = 0; i < length; i += typesCount) {
+// //     typesArr.forEach((type) => {
+// //       const funcName = Object.keys(type)[0];
+// //       generatedPassword += randomFunc[funcName]();
+// //     });
+// //   }
+
+// //   const finalPassword = generatedPassword.slice(0, length);
+
+// //   return finalPassword;
+// // }
