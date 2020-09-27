4dfe7ce89d052cfc1caa0cd0d3bcb877a6ae718b
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-09-27 18:27:05 -0400

    defined variables

--- script.js
+++ script.js
@@ -9,5 +9,13 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Defining varibles
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols = "`~!@#$%^&*()-_=+[]{}|;:',.<>/?";
+var passwordCombination = [];
+var password = "";
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
